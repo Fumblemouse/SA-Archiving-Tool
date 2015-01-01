88400907b38f0d24b536971457d5e5d44a1ae739
Fumblemouse <fumblemouse@gmail.com>
2015-01-02 10:28:25 +1300

    Create dialog.js
    extracted inline JS from archive.html

---
+++ archive/js/dialog.js
@@ -0,0 +1,14 @@
+$(function() {
+	$(".original_link").attr('href', 'http://forums.somethingawful.com/showthread.php?threadid=' + threadid);
+
+	$("#help_dialog").dialog({
+		height:200,
+		autoOpen: false,
+		modal:true
+	});
+	
+	$('.open_help').click(function() {
+		console.log("click");
+		$("#help_dialog").dialog('open');
+	});
+});
